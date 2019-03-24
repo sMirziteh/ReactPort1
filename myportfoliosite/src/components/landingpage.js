24a17d69401ca1b38d35cbcc47a1d0f0b8376332
shelby mirziteh <shelby.mirziteh@gmail.com>
2019-03-23 19:26:24 -0700

    finished react project card

--- myportfoliosite/src/components/landingpage.js
+++ myportfoliosite/src/components/landingpage.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
-
+import { Grid, Cell } from "react-mdl";
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              alt="avatar"
+              className="avatar-img"
+              src="http://www.newdesignfile.com/postpic/2011/02/male-business-avatar-icons_34179.png"
+            />
+            <div className="banner-text">
+              <h1>Full stack web developer</h1>
+              <hr />
+              <p>
+                HTML/CSS/JS | Java | UX/UI | React | Angular | C++ | SQL |
+                Material
+              </p>
+              <div className="social-links">
+                {/*LinkedIn*/}
+                <a
+                  href="https://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                {/*LinkedIn*/}
+                <a
+                  href="https://google.com"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
--- myportfoliosite/src/components/projects.js
+++ myportfoliosite/src/components/projects.js
@@ -1,10 +1,99 @@
 import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+  CardText
+} from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <Card shadow={4} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url('https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png') center / cover"
+            }}
+          >
+            React Project
+          </CardTitle>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            ornare consequat nibh non fermentum. Nullam aliquet mi vel lectus
+            tincidunt.
+          </CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is Angular</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is Java</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1>This is Python</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 4) {
+      return (
+        <div>
+          <h1>This is C++</h1>
+        </div>
+      );
+    }
+  }
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>Java/Spring</Tab>
+          <Tab>Python/Django</Tab>
+          <Tab>C++</Tab>
+        </Tabs>
+
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+        </section>
       </div>
     );
   }
